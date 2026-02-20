07065c9f042b486653bfb22a4d8d363f29988329
Clawdtest-kimi <clawdtest-2548@users.noreply.github.com>
2026-02-20 07:54:07 +0200

    Logo fix

--- inventory-site/my-app/app/components/header.tsx
+++ inventory-site/my-app/app/components/header.tsx
@@ -3,7 +3,6 @@
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-// Logo size updated
 import Image from "next/image";
 
 export function Header() {
