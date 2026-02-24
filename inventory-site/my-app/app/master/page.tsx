c7f471d112219b4d1dbb7ed9c3a6104a0883c1e4
Clawdtest-kimi <clawdtest-2548@users.noreply.github.com>
2026-02-24 09:13:30 +0200

    Add useCallback back for handleFileUpload

--- inventory-site/my-app/app/master/page.tsx
+++ inventory-site/my-app/app/master/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
